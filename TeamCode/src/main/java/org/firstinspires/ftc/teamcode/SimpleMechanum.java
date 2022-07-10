8331d8cbe5c169f2c89d6c2f9264f040a2176e73
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-07-09 20:10:03 -0700

    Push testing opmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleMechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleMechanum.java
@@ -2,12 +2,38 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="SimpleMechanum")
+@TeleOp(name="Testing")
 public class SimpleMechanum extends LinearOpMode {
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor rearLeft;
+    private DcMotor rearRight;
 
+    private void prepareHardware() {
+        // TODO put actual hardware names
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        rearLeft = hardwareMap.get(DcMotor.class, "rear_left");
+        rearRight = hardwareMap.get(DcMotor.class, "rear_right");
+    }
+
+    @Override
+    public void runOpMode() {
+        // tank
+        waitForStart();
+        frontLeft.setPower(1);
+        frontRight.setPower(1);
+        rearLeft.setPower(1);
+        rearRight.setPower(1);
+        sleep(1000);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
     }
 }
