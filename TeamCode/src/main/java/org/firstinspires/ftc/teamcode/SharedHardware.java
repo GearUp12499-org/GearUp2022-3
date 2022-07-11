c016e695b6a16a6178faf8583af266227e0eadab
Liam Doran <65523663+Minerbot360@users.noreply.github.com>
2022-07-10 20:35:19 -0700

    I added things :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SharedHardware.java
@@ -12,25 +12,37 @@ public class SharedHardware {
     public static DcMotor rearLeft;
     public static DcMotor rearRight;
 
+    public static DcMotor encoderLeft;
+    public static DcMotor encoderRight;
+    public static DcMotor encoderRear;
+
     public static void prepareHardware(HardwareMap hardwareMap) {
         // Correct names as of July 9 2022
         frontLeft = hardwareMap.get(DcMotor.class, "front_left");
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         frontRight = hardwareMap.get(DcMotor.class, "front_right");
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         rearLeft = hardwareMap.get(DcMotor.class, "rear_left");
         rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         rearLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         rearRight = hardwareMap.get(DcMotor.class, "rear_right");
         rearRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //copy motors over to encoders
+        encoderLeft = frontLeft;
+        encoderRight = frontRight;
+        encoderRear = rearLeft;
     }
 }
