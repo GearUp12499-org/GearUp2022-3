ea37d77e5ec0df2212cde5e14ee353ee227adcb1
Andrew Chu <chu.andrew.h@gmail.com>
2022-08-06 23:56:04 -0700

    trackwidthtuner, turntest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -50,7 +50,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14; // in
+    public static double TRACK_WIDTH = 14.25; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -92,7 +92,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 60.52801845916335;
     public static double MAX_ACCEL = 60.52801845916335;
-    public static double MAX_ANG_VEL = Math.toRadians(247.7142857142857);
+    public static double MAX_ANG_VEL = 9.345771024505504; // from MaxAngVeloTuner testing // Math.toRadians(247.7142857142857);
     public static double MAX_ANG_ACCEL = Math.toRadians(247.7142857142857);
 
 
