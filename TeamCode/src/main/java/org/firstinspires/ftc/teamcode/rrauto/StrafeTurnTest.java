442a11416000a0267083c141cc43ed724b06aefc
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-09-03 19:07:57 -0700

    Applying old commits...
    (cherry picked from commit b0e78c6fd3f4378c8db916a03d0fb202391315f3)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrauto/StrafeTurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrauto/StrafeTurnTest.java
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode.rrauto;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.localization.Localizer;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Config
 @TeleOp(name="RR: Strafe + Turn Test", group="$RR")
@@ -17,11 +21,11 @@ public class StrafeTurnTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         waitForStart();
-        Trajectory t1 = drive.trajectoryBuilder(new Pose2d(0, 0, 0)).strafeLeft(48).build();
-        drive.followTrajectory(t1);
-        drive.turn(Math.toRadians(180));
-        Trajectory t2 = drive.trajectoryBuilder(new Pose2d(t1.end().getX(), t1.end().getY(), Math.toRadians(180))).strafeLeft(48).build();
-        drive.followTrajectory(t2);
+        TrajectorySequence t1 = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .strafeLeft(48).turn(Math.toRadians(180))
+                .strafeLeft(48).turn(Math.toRadians(180))
+                .build();
+        drive.followTrajectorySequence(t1);
 //        drive.turn(180);
     }
 }
