c89feba61157dc388fa5eb0c2d491b572a28be5c
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-09-18 21:57:33 -0700

    TelemetrySpeed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TelemetrySpeed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TelemetrySpeed.java
@@ -4,23 +4,22 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.IOControl;
 
-@Autonomous(name="Speedtest", group="!test")
-public class Speedtest extends LinearOpMode {
-    public double testTime = 5000; // ms
+@Autonomous(name="TelemetrySpeed", group="!test")
+public class TelemetrySpeed extends LinearOpMode {
+    public static double testTime = 5000; // ms
+
     @Override
     public void runOpMode() throws InterruptedException {
         Telemetry merged = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        IOControl ioControl = new IOControl(hardwareMap);  // connect to the hardware
         merged.addLine("Speedtest");
         merged.addLine();
-        merged.addLine("This will test the poll rate of the distance sensor.");
+        merged.addLine("This will test the poll rate of the telemetry updater.");
         merged.addLine("Please don't press STOP while the test is running.");
         merged.update();
         ElapsedTime e = new ElapsedTime();
@@ -31,7 +30,8 @@ public class Speedtest extends LinearOpMode {
         int count = 0;
         while (opModeIsActive() && e.milliseconds() < testTime) {
             count++;
-            ioControl.distSensor.getDistance(DistanceUnit.MM);
+            merged.addData("c", count);
+            merged.update();
         }
         double duration = e.milliseconds();
         merged.addLine("Counts: " + count + " total");
