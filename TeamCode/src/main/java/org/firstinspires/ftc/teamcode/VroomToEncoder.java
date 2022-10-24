283d0bd2a35ff5259e4c3ade0bc9bfd9aecf5f46
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-10-23 20:01:34 -0700

    vroom vroom

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VroomToEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VroomToEncoder.java
@@ -2,25 +2,31 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="run lift to encoder", group="!!!!!!!!")
+@TeleOp(name="stupid teleop", group="!!!!!!!!")
 public class VroomToEncoder extends LinearOpMode {
-    boolean lastA = false;
-    boolean lastB = false;
+    boolean lastUp = false;
+    boolean lastDown = false;
     @Override
     public void runOpMode() throws InterruptedException {
         Lift lift = new Lift(hardwareMap);
+        Servo servo = hardwareMap.get(Servo.class, "servo");
         waitForStart();
         while (opModeIsActive()) {
+            boolean up = (-gamepad1.left_stick_y) > 0.5;
+            boolean down = (-gamepad1.left_stick_y) < -0.5;
             lift.update();
-            if (gamepad1.a && !lastA) {
+            if (up && !lastUp) {
                 lift.up();
             }
-            if (gamepad1.x && !lastB) {
+            if (down && !lastDown) {
                 lift.down();
             }
-            lastA = gamepad1.a;
-            lastB = gamepad1.x;
+            if (gamepad1.x) servo.setPosition(0);
+            else if (gamepad1.y) servo.setPosition(0.4);
+            lastUp = up;
+            lastDown = down;
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/pipes/MSobelPipeline.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.cv.pipes;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.core.CvType.CV_32FC1;
+
+public class MSobelPipeline extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat tgt = new Mat(input.rows(), input.cols(), CV_32FC1);
+        List<Mat> channels = new ArrayList<>();
+        channels.add(new Mat(input.rows(), input.cols(), CV_32FC1));
+        channels.add(new Mat(input.rows(), input.cols(), CV_32FC1));
+        channels.add(new Mat(input.rows(), input.cols(), CV_32FC1));
+        Core.split(input, channels);
+        // put the channels into variables RGB
+        Mat r = channels.get(0);
+        Mat g = channels.get(1);
+        Mat b = channels.get(2);
+        // apply sobel to each channel
+        Imgproc.Sobel(r, r, CV_32FC1, 1, 0);
+        Imgproc.Sobel(g, g, CV_32FC1, 1, 0);
+        Imgproc.Sobel(b, b, CV_32FC1, 1, 0);
+        // AND the red and green channels
+        Core.divide(255, r, r);
+        Core.divide(255, g, g);
+        Core.divide(255, b, b);
+
+        return tgt;
+    }
+}
