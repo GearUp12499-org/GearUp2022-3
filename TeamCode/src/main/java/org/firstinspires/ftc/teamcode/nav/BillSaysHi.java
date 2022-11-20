2d086efc17bba90023ec0434d8ec3797dd73f718
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2022-11-20 10:44:32 -0800

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nav/BillSaysHi.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.nav;
+
+import static org.firstinspires.ftc.teamcode.SharedHardware.encoderLeft;
+import static org.firstinspires.ftc.teamcode.SharedHardware.encoderRear;
+import static org.firstinspires.ftc.teamcode.SharedHardware.encoderRight;
+import static org.firstinspires.ftc.teamcode.SharedHardware.frontLeft;
+import static org.firstinspires.ftc.teamcode.SharedHardware.frontRight;
+import static org.firstinspires.ftc.teamcode.SharedHardware.prepareHardware;
+import static org.firstinspires.ftc.teamcode.SharedHardware.rearLeft;
+import static org.firstinspires.ftc.teamcode.SharedHardware.rearRight;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.Lift;
+import org.firstinspires.ftc.teamcode.util.NotImplemented;
+
+import java.util.List;
+
+@Autonomous(name = "bill", group = "!!!!!!!!")
+public class BillSaysHi extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        prepareHardware(hardwareMap);
+        EncoderNavigation nav = new EncoderNavigation(
+                frontLeft, frontRight, rearLeft, rearRight, encoderLeft, encoderRight, encoderRear
+        );
+        Paths path = new Paths(nav);
+        Lift lift = new Lift(hardwareMap);
+        nav.dumpTelemetry(telemetry);
+        telemetry.update();
+        lift.openClaw();
+        waitForStart();
+        lift.closeClaw();
+        sleep(500);
+        lift.moveVertical(300);
+        nav.moveForward(24 * 0.75);
+        while (opModeIsActive()) {
+            nav.asyncLoop();
+            nav.dumpTelemetry(telemetry);
+            telemetry.update();
+        }
+    }
+}
