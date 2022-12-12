ee17b8ad1ddc6bc6a6456835b859794ad4126b64
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2022-12-11 22:04:57 -0800

    New pole detection (DetectPoleV2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IOControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IOControl.java
@@ -4,18 +4,11 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class IOControl {
-    public static final String DIST_SENSOR_L_HARDWARE_CONF_NAME = "distanceL";
     public static final String DIST_SENSOR_M_HARDWARE_CONF_NAME = "distance";
-    public static final String DIST_SENSOR_R_HARDWARE_CONF_NAME = "distanceR";
 
-    public DistanceSensor distSensorL;
     public DistanceSensor distSensorM;
-    public DistanceSensor distSensorR;
 
     public IOControl(HardwareMap hardwareMap) {
-        // FIXME attach distance sensors to robot
-//        distSensorL = hardwareMap.get(DistanceSensor.class, DIST_SENSOR_L_HARDWARE_CONF_NAME);
         distSensorM = hardwareMap.get(DistanceSensor.class, DIST_SENSOR_M_HARDWARE_CONF_NAME);
-//        distSensorR = hardwareMap.get(DistanceSensor.class, DIST_SENSOR_R_HARDWARE_CONF_NAME);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Consumer.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+
+/**
+ * Drop in replacement for java.lang.function's Consumer.
+ * Because the accept(...) method isn't in my Android SDK version.
+ * @param <A> input type
+ */
+@FunctionalInterface
+public interface Consumer<A> {
+    void accept(A in);
+}
