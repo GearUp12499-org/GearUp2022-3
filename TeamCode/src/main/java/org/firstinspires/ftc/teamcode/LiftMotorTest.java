4f8c52e654e3e3a9a82ccc723d84340c27f9fbe8
Andrew Chu <chu.andrew.h@gmail.com>
2023-01-12 15:50:01 -0800

    LiftMotorTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftMotorTest.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.SharedHardware.prepareHardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "LiftMotorTest")
+public class LiftMotorTest extends LinearOpMode {
+    public Lift l;
+
+    @Override
+    public void runOpMode() {
+        prepareHardware(hardwareMap);
+        l = new Lift(hardwareMap);
+        l.liftVertical1.setPower(0);
+        l.liftVertical2.setPower(0);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            lift();
+
+            telemetry.update();
+        }
+    }
+
+    public void lift() {
+        l.liftVertical1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        l.liftVertical2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        if (gamepad1.y) {
+            while (l.liftVertical1.getCurrentPosition() < 2200) {
+                l.liftVertical1.setPower(Lift.POWER_UP);
+                l.liftVertical2.setPower(Lift.POWER_UP);
+            }
+            l.liftVertical1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            l.liftVertical2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        else{
+            l.liftVertical1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            l.liftVertical2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            l.liftVertical2.setPower(0);
+            l.liftVertical1.setPower(0);
+        }
+        telemetry.addData("lv1", l.liftVertical1.getCurrentPosition());
+        telemetry.addData("lv2", l.liftVertical2.getCurrentPosition());
+    }
+}
