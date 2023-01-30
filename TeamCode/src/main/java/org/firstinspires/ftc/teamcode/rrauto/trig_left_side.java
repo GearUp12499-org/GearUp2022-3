3532dc2df703538f3ba74f20a9f02f76ba761e76
JJSiu <jamssiu@gmail.com>
2023-01-29 17:36:40 -0800

    trig auto integrated, sag calc integrated

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrauto/trig_left_side.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.rrauto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous(name = "trig_left_side", group = "Pushbot")
+//@Disabled
+public class trig_left_side extends rrAutoComp3 {
+
+    // Constructor
+    public trig_left_side() {
+        position = "trig_left_side";
+    }
+}
