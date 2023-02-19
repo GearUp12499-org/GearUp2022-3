69d5c940f6b048eb9835ff2f7eb3cf14fe8e0d06
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2023-02-18 21:08:06 -0800

    field-centric dri

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele2Op.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele2Op.java
@@ -6,8 +6,11 @@ import static org.firstinspires.ftc.teamcode.SharedHardware.prepareHardware;
 import static org.firstinspires.ftc.teamcode.SharedHardware.rearLeft;
 import static org.firstinspires.ftc.teamcode.SharedHardware.rearRight;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.lib.LinearCleanupOpMode;
 
 @TeleOp(name = "<wip> Tele2Op", group = "!!!!!!!!")
@@ -15,20 +18,38 @@ public class Tele2Op extends LinearCleanupOpMode {
     @Override
     public void main() throws InterruptedException {
         prepareHardware(hardwareMap);
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+        telemetry.addLine("Initializing IMU...");
+        telemetry.update();
+        imu.initialize(parameters);
+        telemetry.addLine("Waiting...");
+        telemetry.update();
         while (opModeInInit()) stopMaybe();
+        telemetry.update();
         waitForStart();
         while (opModeIsActive()) {
             stopMaybe();
+
+            if (gamepad1.start) {
+                imu.resetYaw();
+            }
 
             double y = -gamepad1.left_stick_y; // reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPow = (y + x + rx) / denominator;
-            double backLeftPow = (y - x + rx) / denominator;
-            double frontRightPow = (y - x - rx) / denominator;
-            double rearRightPow = (y + x - rx) / denominator;
+            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            telemetry.addData("he", heading);
+            telemetry.update();
+            double rotX = Math.cos(-heading) * x - Math.sin(-heading) * y;
+            double rotY = Math.sin(-heading) * x + Math.cos(-heading) * y;
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPow = (rotY + rotX + rx) / denominator;
+            double backLeftPow = (rotY - rotX + rx) / denominator;
+            double frontRightPow = (rotY - rotX - rx) / denominator;
+            double rearRightPow = (rotY + rotX - rx) / denominator;
 
             frontLeft.setPower(frontLeftPow);
             rearLeft.setPower(backLeftPow);
